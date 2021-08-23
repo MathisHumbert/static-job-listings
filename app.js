3986666f3535caaa5b4612d3f5f6984d1e10397e
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-23 22:59:42 +0200

    some js added

--- app.js
+++ app.js
@@ -150,3 +150,63 @@ let jobs = [
     tools: ['React', 'Sass'],
   },
 ];
+
+// get elements
+const jobList = document.querySelector('.jobs-list');
+
+// transforming unique skill to skill btn
+let skills = jobs.map((skill) => skill.languages);
+for (let i = 0; i < skills.length; i++) {
+  for (let x = 0; x < skills[i].length; x++) {
+    skills[i][x] = `<button class="skill-btn">${skills[i][x]}</button>`;
+  }
+}
+
+const singleJob = jobs
+  .map((job, index) => {
+    // console.log(job.new ? 'oui' : 'non');
+    // console.log(job.featured ? 'oui' : 'non');
+    skills[index];
+    const [img, name, position, posted, contract, location] = [
+      job.logo,
+      job.company,
+      job.position,
+      job.postedAt,
+      job.contract,
+      job.location,
+    ];
+    const singleSkills = skills[index].join('');
+
+    return `
+  <article class="single-job">
+        <div class="company-info">
+          <img src="${img}" alt="" />
+          <div class="info">
+            <div class="header-info">
+            <p class="name">${name}</p>
+            ${job.new ? `<span class="new">New!</span>` : ''}
+            ${job.featured ? `<span class="featured">Featured</span>` : ''}
+            </div>
+            <p class="job-desc">${position}</p>
+            <div class="footer-info">
+              <p>${posted}</p>
+              <p>${contract}</p>
+              <p>${location}</p>
+            </div>
+          </div>
+          <div class="skills">
+            ${singleSkills}
+          </div>
+        </div>
+      </article>
+  `;
+  })
+  .join('');
+jobList.innerHTML = singleJob;
+
+// tools = skills
+// style css
+// recevoir le name du skill ou tool du btn
+// filter selon le name recu
+// clear et afficher tout
+// function l'affichage
