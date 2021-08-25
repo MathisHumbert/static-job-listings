36ddd959c8ea3dd1b93c9af6af899564161c6da6
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-25 22:48:54 +0200

    css and js added

--- app.js
+++ app.js
@@ -150,34 +150,224 @@ let jobs = [
     tools: ['React', 'Sass'],
   },
 ];
+let jobs2 = [
+  {
+    id: 1,
+    company: 'Photosnap',
+    logo: './images/photosnap.svg',
+    new: true,
+    featured: true,
+    position: 'Senior Frontend Developer',
+    role: 'Frontend',
+    level: 'Senior',
+    postedAt: '1d ago',
+    contract: 'Full Time',
+    location: 'USA Only',
+    languages: ['HTML', 'CSS', 'JavaScript'],
+    tools: [],
+  },
+  {
+    id: 2,
+    company: 'Manage',
+    logo: './images/manage.svg',
+    new: true,
+    featured: true,
+    position: 'Fullstack Developer',
+    role: 'Fullstack',
+    level: 'Midweight',
+    postedAt: '1d ago',
+    contract: 'Part Time',
+    location: 'Remote',
+    languages: ['Python'],
+    tools: ['React'],
+  },
+  {
+    id: 3,
+    company: 'Account',
+    logo: './images/account.svg',
+    new: true,
+    featured: false,
+    position: 'Junior Frontend Developer',
+    role: 'Frontend',
+    level: 'Junior',
+    postedAt: '2d ago',
+    contract: 'Part Time',
+    location: 'USA Only',
+    languages: ['JavaScript'],
+    tools: ['React', 'Sass'],
+  },
+  {
+    id: 4,
+    company: 'MyHome',
+    logo: './images/myhome.svg',
+    new: false,
+    featured: false,
+    position: 'Junior Frontend Developer',
+    role: 'Frontend',
+    level: 'Junior',
+    postedAt: '5d ago',
+    contract: 'Contract',
+    location: 'USA Only',
+    languages: ['CSS', 'JavaScript'],
+    tools: [],
+  },
+  {
+    id: 5,
+    company: 'Loop Studios',
+    logo: './images/loop-studios.svg',
+    new: false,
+    featured: false,
+    position: 'Software Engineer',
+    role: 'Fullstack',
+    level: 'Midweight',
+    postedAt: '1w ago',
+    contract: 'Full Time',
+    location: 'Worldwide',
+    languages: ['JavaScript'],
+    tools: ['Ruby', 'Sass'],
+  },
+  {
+    id: 6,
+    company: 'FaceIt',
+    logo: './images/faceit.svg',
+    new: false,
+    featured: false,
+    position: 'Junior Backend Developer',
+    role: 'Backend',
+    level: 'Junior',
+    postedAt: '2w ago',
+    contract: 'Full Time',
+    location: 'UK Only',
+    languages: ['Ruby'],
+    tools: ['RoR'],
+  },
+  {
+    id: 7,
+    company: 'Shortly',
+    logo: './images/shortly.svg',
+    new: false,
+    featured: false,
+    position: 'Junior Developer',
+    role: 'Frontend',
+    level: 'Junior',
+    postedAt: '2w ago',
+    contract: 'Full Time',
+    location: 'Worldwide',
+    languages: ['HTML', 'JavaScript'],
+    tools: ['Sass'],
+  },
+  {
+    id: 8,
+    company: 'Insure',
+    logo: './images/insure.svg',
+    new: false,
+    featured: false,
+    position: 'Junior Frontend Developer',
+    role: 'Frontend',
+    level: 'Junior',
+    postedAt: '2w ago',
+    contract: 'Full Time',
+    location: 'USA Only',
+    languages: ['JavaScript'],
+    tools: ['Vue', 'Sass'],
+  },
+  {
+    id: 9,
+    company: 'Eyecam Co.',
+    logo: './images/eyecam-co.svg',
+    new: false,
+    featured: false,
+    position: 'Full Stack Engineer',
+    role: 'Fullstack',
+    level: 'Midweight',
+    postedAt: '3w ago',
+    contract: 'Full Time',
+    location: 'Worldwide',
+    languages: ['JavaScript', 'Python'],
+    tools: ['Django'],
+  },
+  {
+    id: 10,
+    company: 'The Air Filter Company',
+    logo: './images/the-air-filter-company.svg',
+    new: false,
+    featured: false,
+    position: 'Front-end Dev',
+    role: 'Frontend',
+    level: 'Junior',
+    postedAt: '1mo ago',
+    contract: 'Part Time',
+    location: 'Worldwide',
+    languages: ['JavaScript'],
+    tools: ['React', 'Sass'],
+  },
+];
+
+// refaire ca
+let roleArr = [];
+let levelArr = [];
+let languagesArr = [];
+let toolsArr = [];
+for (let i of jobs) {
+  languagesArr.push(...i.languages);
+  toolsArr.push(...i.tools);
+  roleArr.push(i.role);
+  levelArr.push(i.level);
+}
+console.log(roleArr);
 
 // get elements
 const jobList = document.querySelector('.jobs-list');
+const filrerList = document.querySelector('.filter-skills');
 
-// transforming unique skill to skill btn
-let skills = jobs.map((skill) => skill.languages);
+// transforming unique skill/tool to skill/tool btn
+let skills = jobs.map((job) => job.languages);
+let tools = jobs.map((job) => job.tools);
 for (let i = 0; i < skills.length; i++) {
   for (let x = 0; x < skills[i].length; x++) {
-    skills[i][x] = `<button class="skill-btn">${skills[i][x]}</button>`;
+    skills[i][
+      x
+    ] = `<button class="skill-btn" data-name="${skills[i][x]}">${skills[i][x]}</button>`;
+  }
+}
+for (let i = 0; i < tools.length; i++) {
+  for (let x = 0; x < tools[i].length; x++) {
+    tools[i][
+      x
+    ] = `<button class="skill-btn" data-name="${tools[i][x]}">${tools[i][x]}</button>`;
   }
 }
 
-const singleJob = jobs
-  .map((job, index) => {
-    // console.log(job.new ? 'oui' : 'non');
-    // console.log(job.featured ? 'oui' : 'non');
-    skills[index];
-    const [img, name, position, posted, contract, location] = [
-      job.logo,
-      job.company,
-      job.position,
-      job.postedAt,
-      job.contract,
-      job.location,
-    ];
-    const singleSkills = skills[index].join('');
+// mapping jobs array
+function displayJobListing(j) {
+  const singleJob = j
+    .map((job, index) => {
+      // destrucutring
+      skills[index];
+      const [img, name, position, posted, contract, location, role, level] = [
+        job.logo,
+        job.company,
+        job.position,
+        job.postedAt,
+        job.contract,
+        job.location,
+        job.role,
+        job.level,
+      ];
+      // unique btn
+      const singleSkills = skills[index];
+      const singleTools = tools[index];
+      const jobBtn = `<button class="skill-btn" data-name="${role}">${role}</button>`;
+      const positionBtn = `<button class="skill-btn" data-name="${level}">${level}</button>`;
+      const allBtn = [
+        ...singleSkills,
+        ...singleTools,
+        jobBtn,
+        positionBtn,
+      ].join('');
 
-    return `
+      // html
+      return `
   <article class="single-job">
         <div class="company-info">
           <img src="${img}" alt="" />
@@ -196,17 +386,54 @@ const singleJob = jobs
           </div>
           </div>
           <div class="skills">
-            ${singleSkills}
+            ${allBtn}
           </div>
       </article>
   `;
-  })
-  .join('');
-jobList.innerHTML = singleJob;
+    })
+    .join('');
+  jobList.innerHTML = singleJob;
+}
 
-// tools = skills
+// push btn value
+let jobFilterArr = [];
+function displayFilter() {
+  // get and push value
+  const value = this.dataset.name;
+  if (!jobFilterArr.includes(value)) {
+    jobFilterArr.push(value);
+  }
+  // displayBtn la faire en fonction
+  if (jobFilterArr.length !== 0) {
+    const filterJobs = jobFilterArr
+      .map((job) => {
+        return `
+      <div class="single-skill">
+        <p>${job}</p>
+        <span class="x" data-name="${job}"><i class="fas fa-times"></i></span>
+      </div>
+      `;
+      })
+      .join('');
+    filrerList.innerHTML = filterJobs;
+  }
+  // clear
+  // remove unique
+  displayAllJobs(jobFilterArr);
+}
+
+function displayAllJobs(arr) {
+  console.log(arr);
+}
+
+// display all
+window.addEventListener('DOMContentLoaded', displayJobListing(jobs));
+// get all btn
+const btns = document.querySelectorAll('.skill-btn');
+// display btn and update all jobs
+btns.forEach((btn) => {
+  btn.addEventListener('click', displayFilter);
+});
+
 // style css
-// recevoir le name du skill ou tool du btn
-// filter selon le name recu
-// clear et afficher tout
-// function l'affichage
+// check si role lang tools et level a une des ces valeurs dans l'arr est check si chaque job2 a une de ses valeur est affiché avec un filter
